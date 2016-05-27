94bb4927159a583e4a4d552bd69db44638d8aa7e
Chris <zc850926@163.com>
2016-05-27 14:10:16 +0800

    add 2 css class and click action

--- action.js
+++ action.js
@@ -1,6 +1,14 @@
-
 $(document).ready(function(e) {
     $('#large').on('click',function() {
 		$('body').addClass('large');
+		$('body').removeClass('narrow');
+	});
+	$('#narrow').on('click',function(){
+		$('body').addClass('narrow');
+		$('body').removeClass('large');
+	});
+	$('#default').on('click',function() {
+		$('body').removeClass('narrow');
+		$('body').removeClass('large');
 	});
 });
