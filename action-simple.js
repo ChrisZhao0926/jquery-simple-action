ae60e007bd6dab4a61127438c52570a719cb5167
Chris <zc850926@163.com>
2016-05-30 17:30:36 +0800

    action propagation

--- action-simple.js
+++ action-simple.js
@@ -1,9 +1,23 @@
 $(document).ready(function(e) {
     $('#default').addClass('selected');
-	$('#switcher button').click(function(){
+	//for the three button
+	$('#switcher button').click(function(event){
 		var bodyClass=this.id;
 		$('body').removeClass().addClass(bodyClass);
 		$('#switcher button').removeClass('selected');
 		$(this).addClass('selected');
+		event.stopPropagation();
+	});
+	//for the hidder three button
+	$('#switcher').click(function(event){
+		//if(event.target==this){
+		$('#switcher button').toggleClass('hidden');
+		//}
+	});
+	//add hover action , 2 paramater, first is in, second is out
+	$('#switcher h3').hover(function(){
+		$(this).addClass('hover');
+	},function(){
+		$(this).removeClass('hover');
 	});
 });
