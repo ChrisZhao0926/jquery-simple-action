5318ce8bde8baf9d00bbb55f29d81625949972c7
Chris <zc850926@163.com>
2016-05-31 16:28:18 +0800

    add delegate js

---
+++ action-delegate.js
@@ -0,0 +1,27 @@
+$(document).ready(function(e) {
+	$('#default').addClass('selected');
+	$('#switcher').hover(function(){
+		$(this).addClass('hover');
+	},function(){
+		$(this).removeClass('hover');
+	});
+    $('#switcher').click(function(event) {
+		if($(event.target).is('button')) {
+			var bodyClass=event.target.id;
+			$('body').removeClass().addClass(bodyClass);
+			$('#switcher button').removeClass('selected');
+			$(event.target).addClass('selected');
+			event.stopPropagation();
+		}else{
+			$('#switcher button').toggleClass('hidden');
+		}
+	});
+	/*$('#switcher').click(function(event){
+
+		if(!$(event.target).is('button')){
+			
+			$('#switcher button').toggleClass('hidden');
+		}
+
+	});*/
+});
