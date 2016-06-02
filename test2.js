2033573bdc31a7f48807a89637bda63bff0bea0a
Chris <zc850926@163.com>
2016-06-02 11:31:49 +0800

    add switch function

--- test2.js
+++ test2.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
     var $speech=$('div.speech');
-    $('#larger').click(function(){
+    var defaultSize=parseFloat($speech.css('fontSize'));
+//    $('#larger').click(function(){
+//        var num=parseFloat($speech.css('fontSize'));
+//        num *=1.2;
+//        $speech.css('fontSize',num+'px');
+//    });
+//    $('#smaller').click(function(){
+//        var num=parseFloat($speech.css('fontSize'));
+//        num *=0.8;
+//        $speech.css('fontSize',num+'px');
+//    });
+    $('#switcher button').click(function(){
         var num=parseFloat($speech.css('fontSize'));
-        num *=1.2;
-        $speech.css('fontSize',num+'px');
-    });
-    $('#smaller').click(function(){
-        var num=parseFloat($speech.css('fontSize'));
-        num *=0.8;
+        switch(this.id){
+            case 'larger':
+                num *=1.4;
+                break;
+            case 'smaller':
+                num /=1.4;
+                break;
+            default:
+                num=defaultSize;
+        }
         $speech.css('fontSize',num+'px');
     });
 });
